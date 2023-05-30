e63bae9c56e0eda8ba0b0e245948006ec9179f89
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-05-30 22:28:18 +0900

    Add files via upload

---
+++ list12-2.c
@@ -0,0 +1,32 @@
+//学生を表す構造体
+
+#include<stdio.h>
+#include<string.h>
+
+#define NAME_LEN 64 //名前の文字数
+
+//学生を表す構造体
+typedef struct {
+	char name[NAME_LEN]; //名前
+	int height; //身長
+	double weight; //体重
+	char blood[NAME_LEN]; //血液型
+}student;
+
+int main(void) {
+
+	student sanaka;
+
+	strcpy_s(sanaka.name, sizeof(sanaka.name), "Sanaka");
+	sanaka.height = 175;
+	sanaka.weight = 62.5;
+	strcpy_s(sanaka.blood, sizeof(sanaka.blood), "A");
+
+	printf("氏名＝%s\n", sanaka.name);
+	printf("身長＝%d\n", sanaka.height);
+	printf("体重＝%.1f\n", sanaka.weight);
+	printf("血液型＝%s\n", sanaka.blood);
+
+	return 0;
+
+}

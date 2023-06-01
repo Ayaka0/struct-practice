c2ab9ccebb6bc2a8570dd57f4587ef2299f0d897
Ayaka0 <47639508+Ayaka0@users.noreply.github.com>
2023-06-01 21:50:20 +0900

    Add files via upload

---
+++ practice1-2.c
@@ -0,0 +1,32 @@
+//構造体を返却する関数
+
+#include<stdio.h>
+
+//xyz構造体
+struct xyz {
+	int x;
+	long y;
+	double z;
+};
+
+//{x,y,z}の値を持つxyz構造体を返却する関数
+struct xyz  xyz_of(int x, long y, double z) {
+	struct xyz temp;
+
+	temp.x = x;
+	temp.y = y;
+	temp.z = z;
+	return temp; //構造体を丸ごと返却
+}
+
+int main(void) {
+	struct xyz s;
+
+	s = xyz_of(12, 7654321, 35.689);
+
+	printf("xyz.x = %d\n", s.x);
+	printf("xyz.y = %ld\n", s.y);
+	printf("xyz.z = %f\n", s.z);
+
+	return 0;
+}
